bcef1b9d43b307290c059bab994c1a87430b3ade
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-05 22:12:12 +0545

    route

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -9,6 +9,6 @@ export async function POST(req: Request){
     const {name} = await req.json();
     users.push({id: users.length + 1, name});
     // return Response.json(users);
-    return new Response(JSON.stringify({mesage: "User added", users.length + 1, name}),{status: 201})
+    return new Response(JSON.stringify({mesage: "User added", id: users.length + 1, name}),{status: 201})
     // return Response.json({message: "User added", name}, {status: 201});
 }
