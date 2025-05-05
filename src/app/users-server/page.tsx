0252cc59417c96d64adaee20f5b1e2b6f47c4bac
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-05 23:04:07 +0545

    fetching server

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,25 @@
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default async function UserServer() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log("Server rendered");
+  return (
+    <ul className="space-y-4 p-4">
+      {users.map((user: User) => (
+        <li
+          key={user.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          {user.name} ({user.email})
+        </li>
+      ))}
+    </ul>
+  );
+}
