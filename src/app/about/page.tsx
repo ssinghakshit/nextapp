bf7dee57466da6183b33cafd97166a0c160f27c5
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-05 20:57:20 +0545

    navigation route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,18 @@
-export default function About(){
-  return <h1>About Akshit</h1>;
-};
+"use client";
+import { useRouter } from "next/navigation";
+export default function About() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>About Akshit</h1>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        Back to Home
+      </button>
+    </div>
+  );
+}
--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,11 +1,33 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const Navigation = ()=>{
-     return (
-        <nav>
-            <Link href="/" className="mr-4 text-blue-500">Home</Link>
-            <Link href="/about" className="mr-4 text-blue-500">About</Link>
-            <Link href="/products/1" className="mr-4 text-blue-500">Product 1</Link>
-        </nav>
-     )
-}
+export const Navigation = () => {
+  const pathname = usePathname();
+  return (
+    <nav>
+      <Link
+        href="/"
+        className={pathname === "/" ? "font-bold mr-4" : "mr-4 text-blue-500"}
+      >
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className={
+          pathname === "/about" ? "font-bold mr-4" : "mr-4 text-blue-500"
+        }
+      >
+        About
+      </Link>
+      <Link
+        href="/products/1"
+        className={
+          pathname.startsWith("/products/1") ? "font-bold mr-4" : "mr-4 text-blue-500"
+        }
+      >
+        Product 1
+      </Link>
+    </nav>
+  );
+};
