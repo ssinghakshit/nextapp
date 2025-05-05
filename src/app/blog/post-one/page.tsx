4a7e345e66863e92ef606b1246902b27d5b9d805
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-05 18:08:19 +0545

    nested route

---
+++ src/app/blog/post-one/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPostOne() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-black-100">
+      <h1 className="text-4xl font-bold mb-4">Blog Post One</h1>
+      <p className="text-lg">This is the content of the first blog post.</p>
+    </div>
+  );
+}
