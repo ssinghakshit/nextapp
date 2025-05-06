abb13db02f0860412f0a7b97aaad1460a67d1a47
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-06 22:08:34 +0545

    use server

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -7,17 +7,49 @@ export default async function mockUsers() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const res = await fetch("https://681a320c1ac11556350830eb.mockapi.io/users");
   const users = await res.json();
+  async function addUser(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const res = await fetch(
+      "https://681a320c1ac11556350830eb.mockapi.io/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    const newUser = await res.json();
+    console.log("new user", newUser);
+  }
   console.log("Server rendered");
   return (
-    <ul className="space-y-4 p-4">
-      {users.map((user: mockUser) => (
-        <li
-          key={user.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+    <div className="py-10">
+      <form action={addUser} className="mb-4">
+        <input
+          type="text"
+          name="name"
+          required
+          className="bg-white border p-2 mr-2 rounded text-gray-900"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
         >
-          {user.name} ({user.id})
-        </li>
-      ))}
-    </ul>
+          Add user
+        </button>
+      </form>
+      <ul className="space-y-4 p-4">
+        {users.map((user: mockUser) => (
+          <li
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            {user.name} ({user.id})
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
