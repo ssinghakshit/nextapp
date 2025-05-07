3ae367377d1b6f6105327149921b6d7fecdb1451
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-07 18:08:57 +0545

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "./components/navigation";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
