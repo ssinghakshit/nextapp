b544dc1a8785b723c984d8da378b290de5323f11
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-07 19:32:06 +0545

    auth

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,13 +1,20 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth, useUser } from "@clerk/nextjs";
 export const Counter = () => {
   // const {isLoaded, userId, sessionId, getToken} = useAuth();
-  const {isLoaded, isSignedIn, user} = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
   console.log("counter log");
   const [count, setCount] = useState(0);
-  if(!isLoaded || !isSignedIn){
+  useEffect(() => {
+    if (!isLoaded || !isSignedIn) {
+      console.log("User not signed in");
+    }
+  },[isLoaded, isSignedIn]);
+  if (!isLoaded || !isSignedIn) {
     return null;
   }
-  return (<button onClick={()=>setCount(count+1)}>Clicked {count} times</button>);
-}
+  return (
+    <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+  );
+};
