60e8a576b741de1683761fa8c1322bd5165578b8
ssinghakshit <akshit.singh2002@gmail.com>
2025-05-07 18:12:28 +0545

    auth

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SignInButton } from "@clerk/nextjs";
 
 export const Navigation = () => {
   const pathname = usePathname();
   return (
-    <nav>
+    <nav className="flex justify-center items-center p-4">
       <Link
         href="/"
         className={pathname === "/" ? "font-bold mr-4" : "mr-4 text-blue-500"}
@@ -23,11 +24,14 @@ export const Navigation = () => {
       <Link
         href="/products/1"
         className={
-          pathname.startsWith("/products/1") ? "font-bold mr-4" : "mr-4 text-blue-500"
+          pathname.startsWith("/products/1")
+            ? "font-bold mr-4"
+            : "mr-4 text-blue-500"
         }
       >
         Product 1
       </Link>
+      <SignInButton mode="modal" />
     </nav>
   );
 };
